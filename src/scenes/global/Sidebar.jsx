17099f0e21445f9de2e3a9515155d907b02bfbf4
Usman-Marwat <us895907a2@gmail.com>
2023-09-05 18:11:49 +0500

    Create winner logs data

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -100,7 +100,7 @@ const SideMenu = () => {
 									fontWeight="bold"
 									sx={{ m: '10px 0 0 0' }}
 								>
-									Alexa
+									Your Name
 								</Typography>
 								<Typography variant="h5" color={colors.greenAccent[500]}>
 									VP Fancy Admin
