44c4060f698c8cc5a5e07ffcab0ec645d1262b9f
Usman-Marwat <us895907a2@gmail.com>
2023-09-17 18:56:26 +0500

    Render otp input for sign up

--- src/scenes/Auth/Signin.jsx
+++ src/scenes/Auth/Signin.jsx
@@ -4,13 +4,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+import LinkMui from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 function Copyright(props) {
 	return (
@@ -21,9 +22,9 @@ function Copyright(props) {
 			{...props}
 		>
 			{'Copyright © '}
-			<Link color="inherit" href="https://mui.com/">
-				Your Website
-			</Link>{' '}
+			<LinkMui color="inherit" href="https://horse-auction.vercel.app/">
+				Horse Auction by Khinsa
+			</LinkMui>{' '}
 			{new Date().getFullYear()}
 			{'.'}
 		</Typography>
@@ -31,8 +32,6 @@ function Copyright(props) {
 }
 
 // TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
 
 export default function SignIn() {
 	const handleSubmit = (event) => {
@@ -45,77 +44,71 @@ export default function SignIn() {
 	};
 
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Container component="main" maxWidth="xs">
-				<CssBaseline />
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-					}}
-				>
-					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component="h1" variant="h5">
-						Sign in
-					</Typography>
-					<Box
-						component="form"
-						onSubmit={handleSubmit}
-						noValidate
-						sx={{ mt: 1 }}
+		<Container component="main" maxWidth="xs">
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in
+				</Typography>
+				<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<FormControlLabel
+						control={<Checkbox value="remember" color="primary" />}
+						label="Remember me"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
 					>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							id="email"
-							label="Email Address"
-							name="email"
-							autoComplete="email"
-							autoFocus
-						/>
-						<TextField
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-						<FormControlLabel
-							control={<Checkbox value="remember" color="primary" />}
-							label="Remember me"
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							sx={{ mt: 3, mb: 2 }}
-						>
-							Sign In
-						</Button>
-						<Grid container>
-							<Grid item xs>
-								<Link href="#" variant="body2">
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item xs>
+							{/* <Link href="#" variant="body2">
 									Forgot password?
-								</Link>
-							</Grid>
-							<Grid item>
-								<Link href="#" variant="body2">
-									{"Don't have an account? Sign Up"}
-								</Link>
-							</Grid>
+								</Link> */}
 						</Grid>
-					</Box>
+						<Grid item>
+							<Link to="/signup">
+								<Typography sx={{ textDecoration: 'underline' }}>
+									Don't have an account? Sign Up
+								</Typography>
+							</Link>
+						</Grid>
+					</Grid>
 				</Box>
-				<Copyright sx={{ mt: 8, mb: 4 }} />
-			</Container>
-		</ThemeProvider>
+			</Box>
+			<Copyright sx={{ mt: 8, mb: 4 }} />
+		</Container>
 	);
 }
--- src/scenes/Auth/Signup.jsx
+++ src/scenes/Auth/Signup.jsx
@@ -3,13 +3,16 @@ import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+import LinkMui from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
+import OtpInput from 'react-otp-input';
+import { useState } from 'react';
+import { Backdrop, ButtonBase, Fade, Modal } from '@mui/material';
 
 function Copyright(props) {
 	return (
@@ -20,20 +23,18 @@ function Copyright(props) {
 			{...props}
 		>
 			{'Copyright © '}
-			<Link color="inherit" href="https://mui.com/">
-				Your Website
-			</Link>{' '}
+			<LinkMui color="inherit" href="https://horse-auction.vercel.app/">
+				Horse Auction by Khinsa
+			</LinkMui>{' '}
 			{new Date().getFullYear()}
 			{'.'}
 		</Typography>
 	);
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
-
 export default function SignUp() {
+	const [open, setOpen] = useState(false);
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		const data = new FormData(event.currentTarget);
@@ -44,99 +45,162 @@ export default function SignUp() {
 	};
 
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Container component="main" maxWidth="xs">
-				<Box
-					sx={{
-						marginTop: 8,
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-					}}
-				>
-					<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component="h1" variant="h5">
-						Sign up
-					</Typography>
-					<Box
-						component="form"
-						noValidate
-						onSubmit={handleSubmit}
-						sx={{ mt: 3 }}
+		<Container component="main" maxWidth="xs">
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign up
+				</Typography>
+				<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete="given-name"
+								name="firstName"
+								required
+								fullWidth
+								id="firstName"
+								label="First Name"
+								autoFocus
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								fullWidth
+								id="lastName"
+								label="Last Name"
+								name="lastName"
+								autoComplete="family-name"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="new-password"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<FormControlLabel
+								control={<Checkbox value="allowExtraEmails" color="primary" />}
+								label="I want to receive inspiration, marketing promotions and updates via email."
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+						onClick={() => setOpen(true)}
 					>
-						<Grid container spacing={2}>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									autoComplete="given-name"
-									name="firstName"
-									required
-									fullWidth
-									id="firstName"
-									label="First Name"
-									autoFocus
-								/>
-							</Grid>
-							<Grid item xs={12} sm={6}>
-								<TextField
-									required
-									fullWidth
-									id="lastName"
-									label="Last Name"
-									name="lastName"
-									autoComplete="family-name"
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									required
-									fullWidth
-									id="email"
-									label="Email Address"
-									name="email"
-									autoComplete="email"
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<TextField
-									required
-									fullWidth
-									name="password"
-									label="Password"
-									type="password"
-									id="password"
-									autoComplete="new-password"
-								/>
-							</Grid>
-							<Grid item xs={12}>
-								<FormControlLabel
-									control={
-										<Checkbox value="allowExtraEmails" color="primary" />
-									}
-									label="I want to receive inspiration, marketing promotions and updates via email."
-								/>
-							</Grid>
-						</Grid>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							sx={{ mt: 3, mb: 2 }}
-						>
-							Sign Up
-						</Button>
-						<Grid container justifyContent="flex-end">
-							<Grid item>
-								<Link href="#" variant="body2">
+						Sign Up
+					</Button>
+					<Grid container justifyContent="flex-end">
+						<Grid item>
+							<Link to="/signin" variant="body2">
+								<Typography sx={{ textDecoration: 'underline' }}>
 									Already have an account? Sign in
-								</Link>
-							</Grid>
+								</Typography>
+							</Link>
 						</Grid>
-					</Box>
+					</Grid>
 				</Box>
-				<Copyright sx={{ mt: 5 }} />
-			</Container>
-		</ThemeProvider>
+			</Box>
+
+			<Copyright sx={{ mt: 5 }} />
+			<OTPModal open={open} setOpen={setOpen} />
+		</Container>
 	);
 }
+
+const OTPModal = ({ open, setOpen }) => {
+	const [otp, setOtp] = useState();
+
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => {
+		// setOpen(false)
+	};
+
+	const style = {
+		position: 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		maxWidth: '90%',
+		minHeight: 200,
+		bgcolor: 'background.paper',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+		borderRadius: 2,
+	};
+	return (
+		<Modal
+			aria-labelledby="transition-modal-title"
+			aria-describedby="transition-modal-description"
+			open={open}
+			onClose={handleClose}
+			closeAfterTransition
+			slots={{ backdrop: Backdrop }}
+			slotProps={{
+				backdrop: {
+					timeout: 500,
+				},
+			}}
+		>
+			<Fade in={open}>
+				<Box sx={style}>
+					<Typography
+						variant="h5"
+						component="h2"
+						textAlign="center"
+						fontWeight="bold"
+					>
+						Enter OTP send to the email
+					</Typography>
+
+					<Box marginTop={5}>
+						<OtpInput
+							value={otp}
+							onChange={setOtp}
+							numInputs={6}
+							renderSeparator={<span>-</span>}
+							renderInput={() => <TextField autoFocus />}
+						/>
+						<Box marginTop={2} textAlign="center">
+							<Button variant="contained" onClick={() => setOpen(false)}>
+								Send
+							</Button>
+						</Box>
+					</Box>
+				</Box>
+			</Fade>
+		</Modal>
+	);
+};
