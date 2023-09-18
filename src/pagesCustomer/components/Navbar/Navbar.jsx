a0396b5a81cdc3d6a66f52fee6dc5091bd73568d
Usman-Marwat <us895907a2@gmail.com>
2023-09-18 19:23:05 +0500

    Logout the user  remove and token

--- src/pagesCustomer/components/Navbar/Navbar.jsx
+++ src/pagesCustomer/components/Navbar/Navbar.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { PiDotsNineBold } from 'react-icons/pi';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import './Navbar.css';
 import logo from '../../../assets/homeImage.png';
 import useAuth from '../../../hooks/useAuth';
+import {
+	Backdrop,
+	Box,
+	Button,
+	CircularProgress,
+	Dialog,
+	DialogActions,
+	DialogTitle,
+	useMediaQuery,
+} from '@mui/material';
+import { useTheme } from '@emotion/react';
 
 function Navbar() {
 	const [navbar, setNavbar] = useState('navbar');
 	const [header, setHeader] = useState('header');
-	const { auth } = useAuth();
+	const [logoutOpen, setLogoutOpen] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const { auth, setAuth } = useAuth();
+	const navigate = useNavigate();
 
 	const showNavbar = () => setNavbar('navbar showNavbar');
 	const removeNavbar = () => setNavbar('navbar');
@@ -19,10 +34,28 @@ function Navbar() {
 		if (window.screenY >= 20) setHeader('header addBg');
 	};
 
+	const handleLogout = () => {
+		setLogoutOpen(false);
+		setLoading(true);
+		setTimeout(() => {
+			setAuth({});
+			localStorage.removeItem('auth');
+			navigate('/', { replace: true });
+			setLoading(false);
+		}, 2000);
+	};
+
 	window.addEventListener('scroll', addBg);
 
 	return (
 		<div className={header}>
+			<Backdrop
+				sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+				open={loading}
+			>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+
 			<Link to={'/'}>
 				<div className="logoDiv flex">
 					<img src={logo} alt="logo img" className="logo" />
@@ -64,18 +97,73 @@ function Navbar() {
 				<AiFillCloseCircle className="icon closeIcon" onClick={removeNavbar} />
 			</div>
 
-			{/* <button className="btn signUpBtn">Sign Up</button> */}
 			<div className="signUp flex">
-				<Link to="/signup">
-					<div className="text">Sign Up</div>
-				</Link>
+				{!auth.role ? (
+					<>
+						<Link to="/signup">
+							<div className="text">Sign Up</div>
+						</Link>
+						<Link to="/signin">
+							<div className="text signin">Sign In</div>
+						</Link>
+					</>
+				) : (
+					<div className="text" onClick={() => setLogoutOpen(true)}>
+						Logout
+					</div>
+				)}
+
 				<PiDotsNineBold
 					className="icon toggleNavbarIcon"
 					onClick={showNavbar}
 				/>
 			</div>
+
+			<LogoutDialog
+				open={logoutOpen}
+				setOpen={setLogoutOpen}
+				onLogout={handleLogout}
+			/>
 		</div>
 	);
 }
+
+const LogoutDialog = ({ open, setOpen, onLogout }) => {
+	const theme = useTheme();
+	const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+	const handleClose = () => setOpen(false);
+
+	return (
+		<Dialog
+			fullScreen={fullScreen}
+			open={open}
+			onClose={handleClose}
+			aria-labelledby="responsive-dialog-title"
+		>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					padding: 2,
+				}}
+			>
+				<LogoutIcon />
+				<DialogTitle id="responsive-dialog-title">
+					Are you sure you want to logout?
+				</DialogTitle>
+				<DialogActions>
+					<Button autoFocus onClick={handleClose}>
+						No
+					</Button>
+					<Button onClick={onLogout} autoFocus>
+						Yes
+					</Button>
+				</DialogActions>
+			</Box>
+		</Dialog>
+	);
+};
 
 export default Navbar;
