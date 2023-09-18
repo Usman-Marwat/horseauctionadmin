4e62a0b02b5668e2c80c2b1001fa40703574fff0
Usman-Marwat <us895907a2@gmail.com>
2023-09-18 19:39:09 +0500

    Show 404 on unauthorized route

--- src/scenes/Missing/index.jsx
+++ src/scenes/Missing/index.jsx
@@ -1,13 +1,34 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import unAuthImage from '../../assets/404.jpg';
+
 function Missing() {
 	return (
-		<div>
-			<Typography>Page not Found</Typography>
-			<Link to="/">Go Back to Home</Link>
-		</div>
+		<Box
+			display="flex"
+			flexDirection="column"
+			justifyContent="center"
+			alignItems="center"
+			height={'80%'}
+		>
+			<Box
+				width={400}
+				height={400}
+				display="flex"
+				flexDirection="column"
+				justifyContent="center"
+				alignItems="center"
+			>
+				<Box component="img" src={unAuthImage} />
+				<Link to="/">
+					<Typography sx={{ textDecoration: 'underline' }}>
+						Go Back to Home
+					</Typography>
+				</Link>
+			</Box>
+		</Box>
 	);
 }
 
